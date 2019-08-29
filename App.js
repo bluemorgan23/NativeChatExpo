913404e80b43c6db840379739426c219af34c722
Chris Morgan <bluemorgan2323@gmail.com>
2019-08-29 16:38:57 -0500

    Chat room working

---
+++ App.js
@@ -0,0 +1,105 @@
+import React, { PureComponent } from 'react';
+import { View, SafeAreaView, Text } from 'react-native';
+import { StreamChat } from 'stream-chat';
+import {
+  Chat,
+  Channel,
+  MessageList,
+  MessageInput,
+  ChannelPreviewMessenger,
+  ChannelList,
+} from 'stream-chat-expo';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+const chatClient = new StreamChat('f8wwud5et5jd');
+const userToken =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoib2xkLXZpb2xldC0yIn0.GCPHGJWTdHiSaeyWLkjYDtXyktY27L3Vc88BdUMtYg4';
+
+const user = {
+  id: 'old-violet-2',
+  name: 'Old violet',
+  image:
+    'https://stepupandlive.files.wordpress.com/2014/09/3d-animated-frog-image.jpg',
+};
+
+chatClient.setUser(user, userToken);
+
+class ChannelListScreen extends PureComponent {
+  static navigationOptions = () => ({
+    headerTitle: (
+      <Text style={{ fontWeight: 'bold' }}>Awesome Conversations</Text>
+    ),
+  });
+
+  render() {
+    return (
+      <SafeAreaView>
+        <Chat client={chatClient}>
+          <View style={{ display: 'flex', height: '100%', padding: 10 }}>
+            <ChannelList
+              filters={{ type: 'messaging', members: { $in: ['old-violet-2'] } }}
+              sort={{ last_message_at: -1 }}
+              Preview={ChannelPreviewMessenger}
+              onSelect={(channel) => {
+                this.props.navigation.navigate('Channel', {
+                  channel,
+                });
+              }}
+            />
+          </View>
+        </Chat>
+      </SafeAreaView>
+    );
+  }
+}
+
+class ChannelScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    const channel = navigation.getParam('channel');
+    return {
+      headerTitle: (
+        <Text style={{ fontWeight: 'bold' }}>{channel.data.name}</Text>
+      ),
+    };
+  };
+
+  render() {
+    const { navigation } = this.props;
+    const channel = navigation.getParam('channel');
+
+    return (
+      <SafeAreaView>
+        <Chat client={chatClient}>
+          <Channel client={chatClient} channel={channel}>
+            <View style={{ display: 'flex', height: '100%' }}>
+              <MessageList />
+              <MessageInput />
+            </View>
+          </Channel>
+        </Chat>
+      </SafeAreaView>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    ChannelList: {
+      screen: ChannelListScreen,
+    },
+    Channel: {
+      screen: ChannelScreen,
+    },
+  },
+  {
+    initialRouteName: 'ChannelList',
+  },
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
